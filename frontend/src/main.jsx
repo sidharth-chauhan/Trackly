b77686b512607bef5be97397b12189a4106831c3
Sidharth chauhan <chauhansiddharth71@gmail.com>
2025-08-10 17:17:18 +0530

    Final fixes for deployment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Trackly/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
