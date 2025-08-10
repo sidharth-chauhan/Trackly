9baab1464ffbf8068627dc993c1a4619f7b52f2e
Sidharth chauhan <chauhansiddharth71@gmail.com>
2025-08-10 12:08:16 +0530

    updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
-import App from './App.jsx'
-import { HashRouter } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter basename="/Trackly">
-      <App />
-    </BrowserRouter>
-    
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter >
+    <App />
+  </BrowserRouter>
+);
